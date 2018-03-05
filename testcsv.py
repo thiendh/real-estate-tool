a8d73bd3982c2774fd1d1f2ed2135923ba02e5c8
Thien Do <thiendh@gmail.com>
2018-03-04 23:03:56 -0800

    check Null

--- testcsv.py
+++ testcsv.py
@@ -33,35 +33,43 @@ def doQuery( conn ) :
 import pymysql
 import csv
 import time
+from sshtunnel import SSHTunnelForwarder
 
-conn = pymysql.connect(host='localhost', port=3306, user='appUser', passwd=password, db=database)
+with SSHTunnelForwarder(
+         ('sp18-cs411-dso-003.cs.illinois.edu', 22),
+         ssh_password="theominhD0",
+         ssh_username="thiendo2",
+         remote_bind_address=('127.0.0.1', 3306)) as server:
 
-cur = conn.cursor()
-insert = 'INSERT INTO `EastWestRealEstateTool`.`test` ('
-replace = ''
-fields = ''
-for field in fieldList:
-    fields = fields + ' `' + field + '`,'
-    replace = replace + '%s, '
+    conn = pymysql.connect(host='localhost', port=server.local_bind_port, user='appUser', passwd=password, db=database)
 
-fields = fields[0: len(fields)-1]
-replace = replace[0:len(replace)-2]
-# sql = sql + fields + ') VALUES (' + replace + ');'
+    cur = conn.cursor()
+    insert = 'INSERT INTO `EastWestRealEstateTool`.`test` ('
+    replace = ''
+    fields = ''
+    for field in fieldList:
+        fields = fields + ' `' + field + '`,'
+        replace = replace + '%s, '
+
+    fields = fields[0: len(fields)-1]
+    replace = replace[0:len(replace)-2]
+    # sql = sql + fields + ') VALUES (' + replace + ');'
 
 
-with open('test.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+    with open('test.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
 
-    for row in reader:
-      data = ''
-      sql = ''
-      for field in fieldList:
-            data= data + "'" + row[field] + "', "
-        # print('Inserting data ', data[0:len(data)-2])
-      sql = insert + fields + ') VALUES (' + data[0:len(data)-2] + ');'
-      print(sql)
-     # time.sleep(5)
-      cur.execute(sql)
+        for row in reader:
+          data = ''
+          sql = ''
+          for field in fieldList:
+                if len(row[field]) == 0: data = data + 'NULL,'
+                else: data= data + "'" + row[field] + "', "
+            # print('Inserting data ', data[0:len(data)-2])
+          sql = insert + fields + ') VALUES (' + data[0:len(data)-2] + ');'
+          print(sql)
+         # time.sleep(5)
+          cur.execute(sql)
 
-cur.close()
-conn.commit()
+    cur.close()
+    conn.commit()
